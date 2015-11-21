225decc40aee67cc61a92558796eabbd054a87de
unknown <>
2015-11-22 00:17:19 +0800

    first commit

---
+++ Course.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.javalist.list;
+
+/**
+ *
+ * @author asus1
+ */
+public class Course {
+    public String id;
+    public String name;
+    public Course (String id,String name){
+      this.id = id;
+      this.name =name;
+      
+    }
+    
+}
